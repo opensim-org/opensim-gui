d2a73f04bf22d2019c664b7e016ef6b186760c84
Ayman Habib <ahabib@stanford.edu>
2012-02-07 19:23:04 +0000

    Enhance script to create TugOfWar model to include creation of muscles
    git-svn-id: file:///dev/shm/opensim/Trunk@6855 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- Gui/opensim/Scripts/testCreateModel.py
+++ Gui/opensim/Scripts/testCreateModel.py
@@ -41,3 +41,39 @@ osimModel.addBody(block);
 
 osimModel.print("tug_of_war02.osim")
 
+maxIsometricForce = 1000.0
+optimalFiberLength = 0.1
+tendonSlackLength = 0.2
+pennationAngle = 0.0
+activation = 0.0001
+deactivation = 1.0
+
+# Create new muscle 1
+muscle1 = modeling.Schutte1993Muscle()
+muscle1.setName("muscle1")
+muscle1.setMaxIsometricForce(maxIsometricForce );
+muscle1.setOptimalFiberLength(optimalFiberLength );
+muscle1.setTendonSlackLength(tendonSlackLength );
+muscle1.setPennationAngleAtOptimalFiberLength(pennationAngle );
+
+muscle2 = modeling.Schutte1993Muscle()
+muscle2.setName("muscle2")
+muscle2.setMaxIsometricForce(maxIsometricForce );
+muscle2.setOptimalFiberLength(optimalFiberLength );
+muscle2.setTendonSlackLength(tendonSlackLength );
+muscle2.setPennationAngleAtOptimalFiberLength(pennationAngle );
+
+
+# Specify the paths for the two muscles
+# Path for muscle 1
+muscle1.addNewPathPoint("muscle1-point1", ground, ArrayDouble.createVec3([0.0,0.05,-0.35]))
+muscle1.addNewPathPoint("muscle1-point2", block, ArrayDouble.createVec3([0.0,0.0,-0.05]));
+# Path for muscle 2
+muscle2.addNewPathPoint("muscle2-point1", ground, ArrayDouble.createVec3([0.0,0.05,0.35]));
+muscle2.addNewPathPoint("muscle2-point2", block, ArrayDouble.createVec3([0.0,0.0,0.05]));
+
+# Add the two muscles (as forces) to the model
+osimModel.addForce(muscle1);
+osimModel.addForce(muscle2);
+
+osimModel.print("tug_of_war03.osim")
