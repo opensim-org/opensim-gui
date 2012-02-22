3dc569f2128085609e96139bf60a26ea4e7b0ec0
Ayman Habib <ahabib@stanford.edu>
2012-02-22 19:32:10 +0000

    More example Scripts
    git-svn-id: file:///dev/shm/opensim/Trunk@6897 fc29be9c-2cb0-420d-b520-73dbd18add6e

---
+++ Gui/opensim/Scripts/runTutorialOne.py
@@ -0,0 +1,22 @@
+import javax.swing as swing
+import org.opensim.modeling as modeling
+import org.opensim.view.pub as view;
+import org.opensim.plotter as plotter;
+
+modeling.OpenSimObject.setDebugLevel(3)
+view.gui.addModel("../../../Models/Tutorials/BothLegs/BothLegs.osim")
+
+view.gui.loadMotion("../../../Models/Tutorials/BothLegs/BothLegsWalk2.mot")
+
+#default pose
+myModel = view.gui.getCurrentModel()
+for i in range(myModel.getCoordinateSet().getSize()):
+	coord = myModel.getCoordinateSet().get(i)
+	view.gui.setCoordinate(coord, coord.getDefaultValue())
+	
+plotterPanel = plotter.OpenSimPlotter.createPlottterPanel("Plot ")
+crv1 = plotter.OpenSimPlotter.addAnalysisCurve(plotterPanel, "fiber-length", "RF", "r_knee_angle")
+plotter.OpenSimPlotter.setCurveLegend(crv1, "RF")
+crv2 = plotter.OpenSimPlotter.addAnalysisCurve(plotterPanel, "fiber-length", "VASINT", "r_knee_angle")
+plotter.OpenSimPlotter.setCurveLegend(crv2, "VASINT")
+plotter.OpenSimPlotter.exportData(plotterPanel, "../../../Models/Tutorials/BothLegs/cvs1.sto")
--- Gui/opensim/Scripts/testPlot.py
+++ Gui/opensim/Scripts/testPlot.py
@@ -4,7 +4,10 @@ import org.opensim.view.pub as view;
 import org.opensim.plotter as plotter;
 import org.opensim.utils as utils;
 
-plotterPanel = plotter.JPlotterPanel()
-f= utils.DialogUtils.createFrameForPanel(plotterPanel, "Plotter")
-plotterPanel.setFrame(f)
-f.setVisible(True)
+plotterPanel = plotter.OpenSimPlotter.createPlottterPanel("Created by Script")
+src = plotter.OpenSimPlotter.addDataSource(plotterPanel, "Scripts/testData/SinglePin_states_degrees.mot")
+crv = plotter.OpenSimPlotter.addCurve(plotterPanel, src, "time", "r1_z")
+crv2 = plotter.OpenSimPlotter.addCurve(plotterPanel, src, "time", "r1_z_u")
+crv.setLegend("my r1_z")
+crv2.setLegend("Speed")
+plotter.OpenSimPlotter.setCurveColor(plotterPanel, 0, 0.0, 1.0, 0.0)
