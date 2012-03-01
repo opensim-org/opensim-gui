65a40b6267b80c475b4a61453a9a34191ec34fe5
Ayman Habib <ahabib@stanford.edu>
2012-03-01 23:52:33 +0000

    Support undo/redo of NewMarkerAction, MarkerDeleteAction
    git-svn-id: file:///dev/shm/opensim/Trunk@6921 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- Gui/opensim/view/src/org/opensim/view/markerEditor/MarkerDeleteAction.java
+++ Gui/opensim/view/src/org/opensim/view/markerEditor/MarkerDeleteAction.java
@@ -3,6 +3,9 @@ package org.opensim.view.markerEditor;
 import java.awt.event.ActionEvent;
 import java.util.Vector;
 import javax.swing.AbstractAction;
+import javax.swing.undo.AbstractUndoableEdit;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
 import org.openide.nodes.Node;
 import org.openide.util.NbBundle;
 import org.opensim.modeling.Marker;
@@ -31,26 +34,50 @@ public class MarkerDeleteAction extends AbstractAction {
         for (int i = 0; i < selected.length; i++) {
             OneMarkerNode objectNode = (OneMarkerNode) selected[i];
             Marker marker = Marker.safeDownCast(objectNode.getOpenSimObject());
-            // Delete the marker's visuals.
-            marker.removeSelfFromDisplay();
-
-            // Remove the marker from the model's marker set.
-            Model model = marker.getBody().getModel();
-            MarkerSet markerset = model.getMarkerSet();
-            markerset.remove(marker);
-
-            // Update the marker name list in the ViewDB.
-            ViewDB.getInstance().getModelGuiElements(model).updateMarkerNames();
-
-            if (true) {
-                // Generate an event so everyone can update, including the marker editor.
-                Vector<OpenSimObject> objs = new Vector<OpenSimObject>(1);
-                objs.add(marker);
-                ObjectsDeletedEvent evnt = new ObjectsDeletedEvent(this, model, objs);
-                OpenSimDB.getInstance().setChanged();
-                OpenSimDB.getInstance().notifyObservers(evnt);
-            }
+            deleteMarker(marker, true);
         }
 
+    }
+
+    public void deleteMarker(final Marker marker, boolean supportUndo) {
+        // Delete the marker's visuals.
+        final String saveMarkerName = marker.getName();
+        final String saveBodyName = marker.getBodyName();
+        final double[] saveMarkerOffset = new double[]{0., 0., 0.};
+        marker.getOffset(saveMarkerOffset);
+        marker.removeSelfFromDisplay();
+
+        // Remove the marker from the model's marker set.
+        final Model model = marker.getBody().getModel();
+        MarkerSet markerset = model.getMarkerSet();
+        markerset.remove(marker);
+
+        // Update the marker name list in the ViewDB.
+        ViewDB.getInstance().getModelGuiElements(model).updateMarkerNames();
+
+        // Generate an event so everyone can update, including the marker editor.
+        Vector<OpenSimObject> objs = new Vector<OpenSimObject>(1);
+        objs.add(marker);
+        ObjectsDeletedEvent evnt = new ObjectsDeletedEvent(this, model, objs);
+        OpenSimDB.getInstance().setChanged();
+        OpenSimDB.getInstance().notifyObservers(evnt);
+        if (supportUndo) {
+            System.out.println("name, body, offset="+saveMarkerName+" "+saveBodyName+" "+saveMarkerOffset);
+            AbstractUndoableEdit auEdit = new AbstractUndoableEdit() {
+
+                public void undo() throws CannotUndoException {
+                    super.undo();
+                    Marker newMarker = model.getMarkerSet().addMarker(saveMarkerName, saveMarkerOffset, model.getBodySet().get(saveBodyName));
+                    new NewMarkerAction().addMarker(newMarker, false);
+                }
+
+                public void redo() throws CannotRedoException {
+                    super.redo();
+                    Marker toDelete = model.getMarkerSet().get(saveMarkerName);
+                    deleteMarker(toDelete, false);
+                }
+            };
+            ExplorerTopComponent.addUndoableEdit(auEdit);
+        }
     }
 }
--- Gui/opensim/view/src/org/opensim/view/markerEditor/NewMarkerAction.java
+++ Gui/opensim/view/src/org/opensim/view/markerEditor/NewMarkerAction.java
@@ -6,13 +6,26 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package org.opensim.view.markerEditor;
 
 import java.awt.event.ActionEvent;
+import java.util.Vector;
 import javax.swing.AbstractAction;
+import javax.swing.undo.AbstractUndoableEdit;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import org.openide.nodes.Node;
 import org.openide.util.NbBundle;
-import org.openide.windows.TopComponent;
+import org.opensim.modeling.Body;
+import org.opensim.modeling.Marker;
+import org.opensim.modeling.MarkerSet;
+import org.opensim.modeling.Model;
+import org.opensim.modeling.OpenSimObject;
+import org.opensim.view.ExplorerTopComponent;
+import org.opensim.view.ObjectsAddedEvent;
+import org.opensim.view.nodes.MarkersNode;
+import org.opensim.view.pub.OpenSimDB;
+import org.opensim.view.pub.ViewDB;
 
 /**
  *
@@ -20,14 +33,77 @@ import org.openide.windows.TopComponent;
  */
 public class NewMarkerAction extends AbstractAction {
 
-   /** Creates a new instance of NewMarkerAction */
-   public NewMarkerAction() {
-      super(NbBundle.getMessage(NewMarkerAction.class, "CTL_NewMarkerAction"));
-   }
+    /** Creates a new instance of NewMarkerAction */
+    public NewMarkerAction() {
+        super(NbBundle.getMessage(NewMarkerAction.class, "CTL_NewMarkerAction"));
+    }
 
-   public void actionPerformed(ActionEvent evt) {
-       MarkerEditorTopComponent win = MarkerEditorTopComponent.findInstance();
-       win.addMarker();
-   }
+    public void actionPerformed(ActionEvent evt) {
+        Node[] selected = ExplorerTopComponent.findInstance().getExplorerManager().getSelectedNodes();
+        // One MarkersNode must have been selected otherwise bail
+        if (selected.length != 1 || !(selected[0] instanceof MarkersNode)) {
+            return;
+        }
+        MarkersNode markersNode = (MarkersNode) selected[0];
+        Model model = markersNode.getModelForNode();
+        double[] offset = {0.11, 0.22, 0.33};
+        MarkerSet markerset = model.getMarkerSet();
+        Body body = model.getBodySet().get(0);
+        if (body == null) {
+            return;
+        }
+        String newMarkerName = makeUniqueMarkerName(markerset);
+        final Marker marker = markerset.addMarker(newMarkerName, offset, body);
+        addMarker(marker, true);
+    }
 
+    public void addMarker(final Marker marker, boolean supportUndo) {
+        // Update the marker name list in the ViewDB.
+        final String saveMarkerName = marker.getName();
+        final String saveBodyName = marker.getBodyName();
+        final double[] saveMarkerOffset = new double[]{0., 0., 0.};
+        marker.getOffset(saveMarkerOffset);
+        final Model model = marker.getBody().getModel();
+        ViewDB.getInstance().getModelGuiElements(model).updateMarkerNames();
+
+        Vector<OpenSimObject> objs = new Vector<OpenSimObject>(1);
+        objs.add(marker);
+        ObjectsAddedEvent evnt = new ObjectsAddedEvent(this, model, objs);
+        OpenSimDB.getInstance().setChanged();
+        OpenSimDB.getInstance().notifyObservers(evnt);
+        // undo support
+        if (supportUndo) {
+            AbstractUndoableEdit auEdit = new AbstractUndoableEdit() {
+
+                public void undo() throws CannotUndoException {
+                    super.undo();
+                    Marker toDelete = model.getMarkerSet().get(saveMarkerName);
+                    new MarkerDeleteAction().deleteMarker(toDelete, false);
+                }
+
+                public void redo() throws CannotRedoException {
+                    super.redo();
+                    Marker newMarker = model.getMarkerSet().addMarker(saveMarkerName, saveMarkerOffset, model.getBodySet().get(saveBodyName));
+                    addMarker(newMarker, false);
+                }
+            };
+            ExplorerTopComponent.addUndoableEdit(auEdit);
+        }
+    }
+
+    private String makeUniqueMarkerName(MarkerSet markerset) {
+        String baseName = "NewMarker";
+        String newMarkerName = baseName;
+        Marker existingMarker = null;
+
+        for (int i = 0; i < markerset.getSize(); i++) {
+            newMarkerName = baseName + "_" + i;
+            if (markerset.contains(newMarkerName) == true) // name is being used by a current marker
+            {
+                continue;
+            }
+        }
+
+        return newMarkerName;
+    }
 }
