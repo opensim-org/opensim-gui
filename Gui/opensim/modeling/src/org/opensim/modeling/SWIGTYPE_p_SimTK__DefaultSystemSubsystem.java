0d699f3456aa74f2597dc5f4e1ce020057aa1995
Ayman Habib <ahabib@stanford.edu>
2012-06-07 18:55:29 +0000

    Remove stale swig generated java files related to Probes and add new file created by swig using latest Java wrapping.
    git-svn-id: file:///dev/shm/opensim/Trunk@7253 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- Gui/opensim/modeling/src/org/opensim/modeling/SWIGTYPE_p_SimTK__DefaultSystemSubsystem.java
+++ Gui/opensim/modeling/src/org/opensim/modeling/SWIGTYPE_p_SimTK__DefaultSystemSubsystem.java
@@ -8,18 +8,18 @@
 
 package org.opensim.modeling;
 
-public class SWIGTYPE_p_OpenSim__ProbeSet {
+public class SWIGTYPE_p_SimTK__DefaultSystemSubsystem {
   private long swigCPtr;
 
-  protected SWIGTYPE_p_OpenSim__ProbeSet(long cPtr, boolean futureUse) {
+  protected SWIGTYPE_p_SimTK__DefaultSystemSubsystem(long cPtr, boolean futureUse) {
     swigCPtr = cPtr;
   }
 
-  protected SWIGTYPE_p_OpenSim__ProbeSet() {
+  protected SWIGTYPE_p_SimTK__DefaultSystemSubsystem() {
     swigCPtr = 0;
   }
 
-  protected static long getCPtr(SWIGTYPE_p_OpenSim__ProbeSet obj) {
+  protected static long getCPtr(SWIGTYPE_p_SimTK__DefaultSystemSubsystem obj) {
     return (obj == null) ? 0 : obj.swigCPtr;
   }
 }
