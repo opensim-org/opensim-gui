c3a51b8a3de4e354ec64d3d569952ddb0fa67836
Ayman Habib <ahabib@stanford.edu>
2012-08-04 00:41:23 +0000

    Remove access to old custom MuscleEditor as unsupported.
    git-svn-id: file:///dev/shm/opensim/Trunk@7458 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- Gui/opensim/view/src/org/opensim/view/nodes/OneMuscleNode.java
+++ Gui/opensim/view/src/org/opensim/view/nodes/OneMuscleNode.java
@@ -74,17 +74,4 @@ public class OneMuscleNode extends OneForceNode {
         return getIcon(i);
     }
 
-    public Action[] getActions(boolean b) {
-        // Get actions from parent (generic object menu for review, display)
-        Action[] superActions = (Action[]) super.getActions(b);
-        // Arrays are fixed size, onvert to a List
-        List<Action> actions = java.util.Arrays.asList(superActions);
-        // Create new Array of proper size
-        Action[] retActions = new Action[actions.size()+1];
-        actions.toArray(retActions);
-        // append new command to the end of the list of actions
-        retActions[actions.size()] = new MuscleEditorAction();
-        return retActions;
-    }
-
 }
