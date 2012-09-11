07099600c0ad32404c17113a35dcfb33fd9ebbb7
Kevin Xu <kevunix@gmail.com>
2012-09-11 00:04:14 +0000

    GUI: Online Doxygen & Users guide are now opened prior to their local copies.
    git-svn-id: file:///dev/shm/opensim/Trunk@7580 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- Gui/opensim/helpUtils/src/org/opensim/helputils/helpmenu/OpenDoxygenAction.java
+++ Gui/opensim/helpUtils/src/org/opensim/helputils/helpmenu/OpenDoxygenAction.java
@@ -9,6 +9,8 @@ package org.opensim.helputils.helpmenu;
  * @author Kevin Xu
  */
 import java.io.File;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
@@ -20,13 +22,21 @@ public final class OpenDoxygenAction extends CallableSystemAction {
     
     public void performAction() {
         String basePath = TheApp.getInstallDir();
-        String doxygenPath = basePath + File.separator + "sdk" + File.separator + "doc" + File.separator + "OpenSimAPI.html";
+        String doxygenPath = "https://simtk.org/api_docs/opensim/api_docs30/";
         System.out.println("PATH: " + doxygenPath);
 
-        // If local file doesn't exist open Doxygen online
-        if(! new File(doxygenPath).exists()) {
-            doxygenPath = "https://simtk.org/api_docs/opensim/api_docs30/";
+        // If issues with online Doxygen then open local Doxygen
+        try {
+            URL url = new URL(doxygenPath);
+            HttpURLConnection urlConnect = (HttpURLConnection)url.openConnection();
+            Object objData = urlConnect.getContent();
+            if(objData == null) {
+                doxygenPath = basePath + File.separator + "sdk" + File.separator + "doc" + File.separator + "OpenSimAPI.html";     
+            }
+        } catch (Exception e) {
+            doxygenPath = basePath + File.separator + "sdk" + File.separator + "doc" + File.separator + "OpenSimAPI.html";          
         }
+
         
         BrowserLauncher.openURL(doxygenPath);
         
--- Gui/opensim/helpUtils/src/org/opensim/helputils/helpmenu/UsersGuideAction.java
+++ Gui/opensim/helpUtils/src/org/opensim/helputils/helpmenu/UsersGuideAction.java
@@ -9,6 +9,8 @@ package org.opensim.helputils.helpmenu;
  * @author Kevin Xu
  */
 import java.io.File;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CallableSystemAction;
@@ -19,14 +21,21 @@ public final class UsersGuideAction extends CallableSystemAction {
     
     public void performAction() {
         String basePath = TheApp.getInstallDir();
-        String usersGuidePath = basePath + File.separator + "sdk" + File.separator + "doc" + File.separator + "UsersGuide.pdf";
+        String usersGuidePath = "http://simtk-confluence.stanford.edu:8080/display/OpenSim/User%27s+Guide"; 
         System.out.println("PATH: " + usersGuidePath);
 
-        // If local file doesn't exist open Doxygen online
-        if(! new File(usersGuidePath).exists()) {
-            usersGuidePath = "http://simtk-confluence.stanford.edu:8080/display/OpenSim/User%27s+Guide";
+        // If issues with online user's guide then open local file
+        try {
+            URL url = new URL(usersGuidePath);
+            HttpURLConnection urlConnect = (HttpURLConnection)url.openConnection();
+            Object objData = urlConnect.getContent();
+            if(objData == null) {
+                usersGuidePath = basePath + File.separator + "sdk" + File.separator + "doc" + File.separator + "UsersGuide.pdf";  
+            }            
+        } catch (Exception e) {
+            usersGuidePath = basePath + File.separator + "sdk" + File.separator + "doc" + File.separator + "UsersGuide.pdf";            
         }
-        
+
         BrowserLauncher.openURL(usersGuidePath);
 
     }
