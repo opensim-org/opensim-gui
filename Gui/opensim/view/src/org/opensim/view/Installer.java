9ead85a889ff2d271e3265f31a288c72601dd4f3
Ayman Habib <ahabib@stanford.edu>
2012-10-26 16:27:51 +0000

    GUI: Update name that displays on GUI builds off trunk to avoid confusion
    git-svn-id: file:///dev/shm/opensim/Trunk@7834 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- Gui/opensim/view/src/org/opensim/view/Installer.java
+++ Gui/opensim/view/src/org/opensim/view/Installer.java
@@ -74,7 +74,7 @@ public class Installer extends ModuleInstall {
 
     public void restored() {
         super.restored();
-        System.setProperty ("netbeans.buildnumber", "3.0.beta7"); // Should get that from JNI but sometimes doesn't work'
+        System.setProperty ("netbeans.buildnumber", "3.0+"); // Should get that from JNI but sometimes doesn't work'
         try {
              // Put your startup code here.
             UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
