ae697a441c815e5561c8960367c1f7313cc8e187
Ayman Habib <ahabib@stanford.edu>
2013-11-11 19:06:22 +0000

    GUI: Add new file missing from commit rev. 8521.
    git-svn-id: file:///dev/shm/opensim/Trunk@8527 fc29be9c-2cb0-420d-b520-73dbd18add6e

---
+++ Gui/opensim/view/src/org/opensim/view/actions/ObjectDisplaySelectAction.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c)  2005-2008, Stanford University
+ * Use of the OpenSim software in source form is permitted provided that the following
+ * conditions are met:
+ * 	1. The software is used only for non-commercial research and education. It may not
+ *     be used in relation to any commercial activity.
+ * 	2. The software is not distributed or redistributed.  Software distribution is allowed 
+ *     only through https://simtk.org/home/opensim.
+ * 	3. Use of the OpenSim software or derivatives must be acknowledged in all publications,
+ *      presentations, or documents describing work in which OpenSim or derivatives are used.
+ * 	4. Credits to developers may not be removed from executables
+ *     created from modifications of the source.
+ * 	5. Modifications of source code must retain the above copyright notice, this list of
+ *     conditions and the following disclaimer. 
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ *  SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR BUSINESS INTERRUPTION) OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ *  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.opensim.view.actions;
+
+import javax.swing.SwingUtilities;
+import org.openide.nodes.Children;
+import org.openide.nodes.Node;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.CallableSystemAction;
+import org.opensim.view.ExplorerTopComponent;
+import org.opensim.view.nodes.OpenSimObjectNode;
+import org.opensim.view.nodes.OpenSimObjectSetNode;
+import org.opensim.view.pub.ViewDB;
+
+public final class ObjectDisplaySelectAction extends CallableSystemAction  {
+
+   public ObjectDisplaySelectAction() {
+   }
+
+   public String getName() {
+      return NbBundle.getMessage(ObjectDisplaySelectAction.class, "CTL_SelectObjectAction");
+   }
+
+    @Override
+    public void performAction() {
+        SwingUtilities.invokeLater(new Runnable() {
+
+            public void run() {
+                Node[] selected = ExplorerTopComponent.findInstance().getExplorerManager().getSelectedNodes();
+                // The general scenario is to hide parent then show node and children
+                // one exception is if the parent node is a model since hiding it in this case
+                // makes it impossible to show selected node. This sitution is handled by overriding the behavior
+                // in appropriate nodes.
+                for (int i = 0; i < selected.length; i++) {
+                    
+                    if (selected[i] instanceof OpenSimObjectNode){ 
+                        OpenSimObjectNode osNode = (OpenSimObjectNode) selected[i];
+                        ViewDB.getInstance().setSelectedObject(osNode.getOpenSimObject());
+                    }
+                 }
+            }
+        });
+    }
+
+    @Override
+    public HelpCtx getHelpCtx() {
+        return HelpCtx.DEFAULT_HELP;
+    }
+
+}
