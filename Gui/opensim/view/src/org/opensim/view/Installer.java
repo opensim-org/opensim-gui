f12e12918aa70cdd3af5330633b1ed21ba1fd9cc
Ayman Habib <ahabib@stanford.edu>
2013-11-22 18:32:23 +0000

    Change display version in GUI to 3.2 in prep for branching.
    git-svn-id: file:///dev/shm/opensim/Trunk@8535 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- Gui/opensim/view/src/org/opensim/view/Installer.java
+++ Gui/opensim/view/src/org/opensim/view/Installer.java
@@ -74,7 +74,7 @@ public class Installer extends ModuleInstall {
 
     public void restored() {
         super.restored();
-        System.setProperty ("netbeans.buildnumber", "3.1"); // Should get that from JNI but sometimes doesn't work'
+        System.setProperty ("netbeans.buildnumber", "3.2"); // Should get that from JNI but sometimes doesn't work'
         try {
              // Put your startup code here.
             UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
