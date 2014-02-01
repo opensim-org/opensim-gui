fb168e6ebcf143c937f1145101e1bdfdbf61914f
Christopher Dembia <cld72@cornell.edu>
2014-02-01 00:45:14 +0000

    Update createControlsFromStorage.py so that it:
    * produces a controls XML file that need not be edited before use in a simulation (by appending '.excitation' to Control names).
    * gives a message informing the user about a potential issue with unspecified min/max values.
    
    git-svn-id: file:///dev/shm/opensim/Branches/OpenSim32@8653 fc29be9c-2cb0-420d-b520-73dbd18add6e

--- Gui/opensim/Scripts/createControlsFromStorage.py
+++ Gui/opensim/Scripts/createControlsFromStorage.py
@@ -40,13 +40,13 @@ offsetIfMinMax_Unspecified=0.01
 # Prompts user to select file if above does not exist
 controlProfileStore = "doesnotexistfile.sto"
 if not os.path.exists(controlProfileStore):
-	controlProfileStore = utils.FileUtils.getInstance().browseForFilename(".sto", "Select the storage file for controls profile", 1)
+    controlProfileStore = utils.FileUtils.getInstance().browseForFilename(".sto", "Select the storage file for controls profile", 1)
 print controlProfileStore
 controlStorage = modeling.Storage(controlProfileStore)
 
 controlProfileXml = "doesnotexistfile.xml"
 if not os.path.exists(controlProfileXml):
-	controlProfileXml = utils.FileUtils.getInstance().browseForFilenameToSave(".xml", "Select the xml file for controls", 1, "controls.xml")
+    controlProfileXml = utils.FileUtils.getInstance().browseForFilenameToSave(".xml", "Select the xml file for controls", 1, "controls.xml")
 print controlProfileXml
 
 #Invoke constructor of ControlSet that takes a Storage as input.
@@ -54,10 +54,10 @@ print controlProfileXml
 csetRaw = modeling.ControlSet(controlStorage)
 
 # Now cycle thru the controls and foreach
-#	find name, if not ending in _min or _max then
-#	if name_min exists then use it as minValue otherwise use value itself-offsetIfMinMax_Unspecified as minValue
-#	if name_max exists then use it as maxValue otherwise use value itself+offsetIfMinMax_Unspecified as maxValue
-#	remove all controls that end in _min or _max and write to file
+#    find name, if not ending in _min or _max then
+#    if name_min exists then use it as minValue otherwise use value itself-offsetIfMinMax_Unspecified as minValue
+#    if name_max exists then use it as maxValue otherwise use value itself+offsetIfMinMax_Unspecified as maxValue
+#    remove all controls that end in _min or _max and write to file
 
 # number of controls 
 sz = csetRaw.getSize()
@@ -65,78 +65,119 @@ sz = csetRaw.getSize()
 lbls = controlStorage.getColumnLabels()
 nLabels = lbls.getSize()-1
 
+someMinMaxNotSpecified = False
+
 #for all controls in the set do this
 for i in range (0, nLabels):
-	csi = csetRaw.get(i)
-	csiL = modeling.ControlLinear.safeDownCast(csi)
-	controlName = csi.getName()
-	minControlName = controlName+'_min'
-	minControlIndex = -1
-	maxControlName = controlName+'_max'
-	maxControlIndex = -1
-	# if name doesn't contain _min or _max
-	if (controlName.find('_min')==-1 and controlName.find('_max')==-1):
-		minControlIndex = csetRaw.getIndex(minControlName)
-		maxControlIndex = csetRaw.getIndex(maxControlName)
-		currentMin = 10000000
-		currentMax = -10000000
-		if (minControlIndex!= -1):
-			csiL_min = modeling.ControlLinear.safeDownCast(csetRaw.get(minControlIndex))
-			nodeSet = csiL.getControlValues()
-			nodeSetSize = nodeSet.getSize()
-			for nodeNum in range(0, nodeSetSize-1):
-				clNode = nodeSet.get(nodeNum)
-				clNodeTime = clNode.getTime()
-				minValue = csiL_min.getControlValue(clNodeTime)
-				csiL.setControlValueMin(clNodeTime, minValue)
-				if (minValue < currentMin):
-					currentMin = minValue
-		else:
-			nodeSet = csiL.getControlValues()
-			nodeSetSize = nodeSet.getSize()
-			for nodeNum in range(0, nodeSetSize-1):
-				clNode = nodeSet.get(nodeNum)
-				clNodeTime = clNode.getTime()
-				minValue = csiL.getControlValue(clNodeTime);
-				csiL.setControlValueMin(clNodeTime, minValue-offsetIfMinMax_Unspecified)
-				if (minValue < currentMin):
-					currentMin = minValue
-			currentMin=currentMin-offsetIfMinMax_Unspecified
-		csiL.setDefaultParameterMin(currentMin)
-		# repeat for max
-		if (maxControlIndex!=-1):
-			csiL_max = modeling.ControlLinear.safeDownCast(csetRaw.get(maxControlIndex))
-			nodeSet = csiL.getControlValues()
-			nodeSetSize = nodeSet.getSize()
-			for nodeNum in range(0, nodeSetSize-1):
-				clNode = nodeSet.get(nodeNum)
-				clNodeTime = clNode.getTime()
-				maxValue = csiL_max.getControlValue(clNodeTime)
-				csiL.setControlValueMax(clNodeTime, maxValue)
-				if (maxValue > currentMax):
-					currentMax = maxValue
-		else:
-			nodeSet = csiL.getControlValues()
-			nodeSetSize = nodeSet.getSize()
-			for nodeNum in range(0, nodeSetSize-1):
-				clNode = nodeSet.get(nodeNum)
-				clNodeTime = clNode.getTime()
-				maxValue = csiL.getControlValue(clNodeTime)
-				csiL.setControlValueMax(clNodeTime, maxValue+offsetIfMinMax_Unspecified)
-				if (maxValue > currentMax):
-					currentMax = maxValue
-			currentMax = currentMax+offsetIfMinMax_Unspecified
-		csiL.setDefaultParameterMax(currentMax)
+    csi = csetRaw.get(i)
+    csiL = modeling.ControlLinear.safeDownCast(csi)
+    controlName = csi.getName()
+    csi.setName(controlName + '.excitation')
+    minControlName = controlName+'_min'
+    minControlIndex = -1
+    maxControlName = controlName+'_max'
+    maxControlIndex = -1
+    # if name doesn't contain _min or _max
+    if (controlName.find('_min')==-1 and controlName.find('_max')==-1):
+        minControlIndex = csetRaw.getIndex(minControlName)
+        maxControlIndex = csetRaw.getIndex(maxControlName)
+        currentMin = 10000000
+        currentMax = -10000000
+        if (minControlIndex!= -1):
+            csiL_min = modeling.ControlLinear.safeDownCast(csetRaw.get(minControlIndex))
+            nodeSet = csiL.getControlValues()
+            nodeSetSize = nodeSet.getSize()
+            for nodeNum in range(0, nodeSetSize-1):
+                clNode = nodeSet.get(nodeNum)
+                clNodeTime = clNode.getTime()
+                minValue = csiL_min.getControlValue(clNodeTime)
+                csiL.setControlValueMin(clNodeTime, minValue)
+                if (minValue < currentMin):
+                    currentMin = minValue
+        else:
+            someMinMaxNotSpecified = True
+            nodeSet = csiL.getControlValues()
+            nodeSetSize = nodeSet.getSize()
+            for nodeNum in range(0, nodeSetSize-1):
+                clNode = nodeSet.get(nodeNum)
+                clNodeTime = clNode.getTime()
+                minValue = csiL.getControlValue(clNodeTime);
+                csiL.setControlValueMin(clNodeTime, minValue-offsetIfMinMax_Unspecified)
+                if (minValue < currentMin):
+                    currentMin = minValue
+            currentMin=currentMin-offsetIfMinMax_Unspecified
+        csiL.setDefaultParameterMin(currentMin)
+        # repeat for max
+        if (maxControlIndex!=-1):
+            csiL_max = modeling.ControlLinear.safeDownCast(csetRaw.get(maxControlIndex))
+            nodeSet = csiL.getControlValues()
+            nodeSetSize = nodeSet.getSize()
+            for nodeNum in range(0, nodeSetSize-1):
+                clNode = nodeSet.get(nodeNum)
+                clNodeTime = clNode.getTime()
+                maxValue = csiL_max.getControlValue(clNodeTime)
+                csiL.setControlValueMax(clNodeTime, maxValue)
+                if (maxValue > currentMax):
+                    currentMax = maxValue
+        else:
+            someMinMaxNotSpecified = True
+            nodeSet = csiL.getControlValues()
+            nodeSetSize = nodeSet.getSize()
+            for nodeNum in range(0, nodeSetSize-1):
+                clNode = nodeSet.get(nodeNum)
+                clNodeTime = clNode.getTime()
+                maxValue = csiL.getControlValue(clNodeTime)
+                csiL.setControlValueMax(clNodeTime, maxValue+offsetIfMinMax_Unspecified)
+                if (maxValue > currentMax):
+                    currentMax = maxValue
+            currentMax = currentMax+offsetIfMinMax_Unspecified
+        csiL.setDefaultParameterMax(currentMax)
 
 #remove entries that has trailing _min or _max
 for i in range (0, lbls.getSize()):
-	label = lbls.getitem(i)
-	if (label.find('_min')==-1 and label.find('_max')==-1):
-		continue
-	idx = csetRaw.getIndex(label)
-	csetRaw.remove(idx)
+    label = lbls.getitem(i)
+    if (label.find('_min')==-1 and label.find('_max')==-1):
+        continue
+    idx = csetRaw.getIndex(label)
+    csetRaw.remove(idx)
 
 # Write output to xml file
 csetRaw.print(controlProfileXml)
+
+#Popup a dialog to show the file name used to save the model
+win = swing.JFrame("Information")
+printMsg = ("Wrote controls XML file to <b>" + controlProfileXml +
+            "</b>.<br><br>")
+if offsetIfMinMax_Unspecified != 0 and someMinMaxNotSpecified:
+    import inspect
+    thisFilePath = inspect.getfile(inspect.currentframe())
+    offsetMsg = ("OpenSim (i.e., CMC) may choose control "
+            "values<br>" +
+            "that are <b>+/-" + str(offsetIfMinMax_Unspecified) + 
+            "</b> of your specified control values if you did not "
+            "specify min/max<br>" +
+            "control values in <b>" + controlProfileStore + "</b>.<br>"
+            "This could be "
+            "an issue if, for example, your actuator(s) can only apply<br>"
+            "positive actuation and one of your control nodes specifies<br>"
+            "a value of 0.0. If you did not appropriately set the<br>"
+            "Actuator's min_control/max_control, then OpenSim may<br>"
+            "choose a negative control. You can change this by editing<br>"
+            "<b>offsetIfMinMax_Unspecified</b> in <br><b>" + thisFilePath +
+            "</b>.<br><br>")
+else:
+    offsetMsg = ""
+excitMsg = ("In <b>" + controlProfileXml + "</b>, we have named the "
+        "Control objects as "
+        "<b>&lt;Actuator-name&gt;.excitation.<br></b>This is done so the "
+        "Control objects are properly "
+        "associated with your Actuator(s).<br><br>")
+        
+dLabel = swing.JLabel("<html><br>" + printMsg + offsetMsg + 
+        excitMsg + "</br></html>")
+        
+win.getContentPane().add(dLabel)
+win.pack()
+win.show()
 
 
