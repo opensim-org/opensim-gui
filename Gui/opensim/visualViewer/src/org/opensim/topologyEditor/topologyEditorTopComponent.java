81eb91de7302f66f02e836ca610010585b0467a9
Ayman Habib <ahabib@stanford.edu>
2015-03-06 17:43:43 -0800

    Fix Topology Viewer per latest version of Visual Library.

--- Gui/opensim/visualViewer/src/org/opensim/topologyEditor/topologyEditorTopComponent.java
+++ Gui/opensim/visualViewer/src/org/opensim/topologyEditor/topologyEditorTopComponent.java
@@ -130,11 +130,11 @@ public final class topologyEditorTopComponent extends TopComponent implements Ob
     private void populateDefault() {
         GraphLayoutSupport.setTreeGraphLayoutRootNode (graphLayout, "ground");
         final SceneLayout sceneGraphLayout = LayoutFactory.createSceneGraphLayout (scene, graphLayout);
-        
         WidgetAction editAction = ActionFactory.createEditAction (new MyEditProvider (sceneGraphLayout));
         scene.getActions().addAction (editAction);
         currentModel = OpenSimDB.getInstance().getCurrentModel();
         if (currentModel == null) return;
+        scene.addNode("ground");
         JointSet jnts = currentModel.getJointSet();
         int numJoints = jnts.getSize();
         for (int j=0; j<numJoints; j++ ){
