1407c6951622a46be5d8d56c823e8e6b8d3577a7
Ayman Habib <ahabib@stanford.edu>
2015-09-28 11:21:07 -0700

    Exporter threejs in progress.

--- Gui/opensim/view/src/org/opensim/threejs/DecorativeGeometryImplementationJS.java
+++ Gui/opensim/view/src/org/opensim/threejs/DecorativeGeometryImplementationJS.java
@@ -6,6 +6,9 @@
 
 package org.opensim.threejs;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.UUID;
 import org.json.simple.JSONArray;
 import org.opensim.modeling.DecorativeArrow;
 import org.opensim.modeling.DecorativeBrick;
@@ -14,6 +17,7 @@ import org.opensim.modeling.DecorativeCone;
 import org.opensim.modeling.DecorativeCylinder;
 import org.opensim.modeling.DecorativeEllipsoid;
 import org.opensim.modeling.DecorativeFrame;
+import org.opensim.modeling.DecorativeGeometry;
 import org.opensim.modeling.DecorativeGeometryImplementation;
 import org.opensim.modeling.DecorativeLine;
 import org.opensim.modeling.DecorativeMesh;
@@ -22,85 +26,123 @@ import org.opensim.modeling.DecorativePoint;
 import org.opensim.modeling.DecorativeSphere;
 import org.opensim.modeling.DecorativeText;
 import org.opensim.modeling.DecorativeTorus;
+import org.opensim.modeling.OpenSimObject;
+import org.opensim.modeling.Vec3;
 
 /**
  *
  * @author Ayman
  */ 
 public class DecorativeGeometryImplementationJS extends DecorativeGeometryImplementation {
-    JSONArray jsona = new JSONArray();
+    private JSONArray jsonArr;
+    private UUID geomID;
     
-    public void initJSON() {
-        jsona = new JSONArray();
+    public DecorativeGeometryImplementationJS(JSONArray jsonArr) {
+        this.jsonArr = jsonArr;
     }
+    
     @Override
     public void implementConeGeometry(DecorativeCone arg0) {
-        super.implementConeGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        //super.implementConeGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void implementArrowGeometry(DecorativeArrow arg0) {
-        super.implementArrowGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        //super.implementArrowGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void implementTorusGeometry(DecorativeTorus arg0) {
-        super.implementTorusGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        //super.implementTorusGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void implementMeshFileGeometry(DecorativeMeshFile arg0) {
-        super.implementMeshFileGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        //super.implementMeshFileGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void implementMeshGeometry(DecorativeMesh arg0) {
-        super.implementMeshGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        //super.implementMeshGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void implementTextGeometry(DecorativeText arg0) {
-        super.implementTextGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        //super.implementTextGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void implementFrameGeometry(DecorativeFrame arg0) {
-        super.implementFrameGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        //json.put("name", arg0.getUserRefAsObject());
+        //super.implementFrameGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void implementEllipsoidGeometry(DecorativeEllipsoid arg0) {
-        super.implementEllipsoidGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        //super.implementEllipsoidGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void implementSphereGeometry(DecorativeSphere arg0) {
-        super.implementSphereGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        Map dg_json = new LinkedHashMap();
+        dg_json.put("uuid", geomID);
+        dg_json.put("type", "SphereGeometry");
+	dg_json.put("radius", arg0.getRadius());
+	dg_json.put("radialSegments", 32);
+	dg_json.put("heightSegments", 1);
+        setTransformAndScale(dg_json, arg0);
+        String temp=dg_json.toString();
+        jsonArr.add(dg_json);        
     }
 
     @Override
     public void implementCircleGeometry(DecorativeCircle arg0) {
-        super.implementCircleGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        //super.implementCircleGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void implementCylinderGeometry(DecorativeCylinder arg0) {
-        super.implementCylinderGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        Map dg_json = new LinkedHashMap();
+        dg_json.put("uuid", geomID);
+        dg_json.put("type", "CylinderGeometry");
+	dg_json.put("radiusTop", arg0.getRadius());
+	dg_json.put("radiusBottom", arg0.getRadius());
+	dg_json.put("height", arg0.getHalfHeight()*2);
+	dg_json.put("radialSegments", 32);
+	dg_json.put("heightSegments", 1);
+        setTransformAndScale(dg_json, arg0);
+        String temp=dg_json.toString();
+        jsonArr.add(dg_json);        
+    }
+
+    private void setTransformAndScale(Map dg_json, DecorativeGeometry arg0) {
+        dg_json.put("position", new Vec3(arg0.getTransform().p()));
+        dg_json.put("rotation", new Vec3(arg0.getTransform().R().convertRotationToBodyFixedXYZ()));
+        dg_json.put("scale", arg0.getScaleFactors());
     }
 
     @Override
     public void implementBrickGeometry(DecorativeBrick arg0) {
-        super.implementBrickGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        //super.implementBrickGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void implementLineGeometry(DecorativeLine arg0) {
-        super.implementLineGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        //super.implementLineGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void implementPointGeometry(DecorativePoint arg0) {
-        super.implementPointGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
+        //super.implementPointGeometry(arg0); //To change body of generated methods, choose Tools | Templates.
     }
-    
+    /**
+     * @return the jsonArr
+     */
+    public JSONArray getJsonArr() {
+        return jsonArr;
+    }
+
+    void setGeomID(UUID hexGeom) {
+        geomID = hexGeom;
+    }
 }
--- Gui/opensim/view/src/org/opensim/threejs/ExportSceneToThreeJsAction.java
+++ Gui/opensim/view/src/org/opensim/threejs/ExportSceneToThreeJsAction.java
@@ -5,20 +5,31 @@
  */
 package org.opensim.threejs;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.UUID;
 import org.json.simple.JSONArray;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
 import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
+import org.opensim.modeling.Appearance;
 import org.opensim.modeling.ArrayDecorativeGeometry;
+import org.opensim.modeling.Component;
+import org.opensim.modeling.ComponentIterator;
+import org.opensim.modeling.ComponentsList;
 import org.opensim.modeling.DecorativeGeometry;
 import org.opensim.modeling.Model;
 import org.opensim.modeling.ModelDisplayHints;
@@ -40,51 +51,119 @@ public final class ExportSceneToThreeJsAction implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         BufferedWriter out = null;
         try {
+            //HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+            //server.createContext("/scene", new MyHandler());
+            //server.setExecutor(null); // creates a default executor
+            //server.start();
+            //BrowserLauncher.openURL("http://localhost:8000/scene/v0.html");
+            out = new BufferedWriter(new FileWriter("C:\\Dev\\gui\\opensim-gui\\Gui\\opensim\\scene\\scene.json", false));
             // TODO implement action body
             Model model = OpenSimDB.getInstance().getCurrentModel();
             ModelDisplayHints mdh = model.getDisplayHints();
-            ArrayDecorativeGeometry adg = new ArrayDecorativeGeometry();
-            model.generateDecorations(true, mdh, model.getWorkingState(), adg);
+            ComponentsList mcList = model.getComponentsList();
+            ComponentIterator mcIter = mcList.begin();
             // This should be initialized from default visResources/mt_scene.js
-            JSONArray jsona = new JSONArray();
-            if (adg.size()>0){  // Component has some geometry
-                DecorativeGeometry dg;
-                for (int idx=0; idx <adg.size(); idx++){
-                    dg = adg.getElt(idx);
-                    Map dg_json=new LinkedHashMap();
-                    /*
-                                        "geometry" : "cubeNormals",
-					"material" : "phong_red",
-					"position" : [ 0, 0, 0 ],
-					"rotation" : [ 0, -0.3, 0 ],
-					"scale"	   : [ 1, 1, 1 ],
-					"visible"  : true,
-                    */
-                    dg_json.put("geomerty","Geometry_"+String.valueOf(idx));
-                    dg_json.put("position",new Vec3(dg.getTransform().p()));
-                    dg_json.put("rotation",new Vec3(dg.getTransform().R().convertRotationToBodyFixedXYZ()));
-                    dg_json.put("scale",dg.getScaleFactors());
-                    jsona.add(dg_json);
+            JSONArray json_geometries = new JSONArray();
+            HashMap<String, UUID> mapGeometryToUUID = new HashMap<String, UUID>() {};
+            
+            JSONArray json_materials = new JSONArray();
+            HashMap<String, UUID> mapMaterialToUUID = new HashMap<String, UUID>() {};
+
+            DecorativeGeometryImplementationJS dgimp = new DecorativeGeometryImplementationJS(json_geometries);
+            while (!mcIter.equals(mcList.end())) {
+                //System.out.println("Object:Type,Name:"+ mcIter.getConcreteClassName()+","+mcIter.getName());
+                Component comp = mcIter.__deref__();
+                System.out.println("Comp:"+comp.getConcreteClassName()+"ID="+comp.getPathName());
+                ArrayDecorativeGeometry adg = new ArrayDecorativeGeometry();
+                comp.generateDecorations(true, mdh, model.getWorkingState(), adg);
+                comp.generateDecorations(false, mdh, model.getWorkingState(), adg);
+                if (adg.size() > 0) {  // Component has some geometry
+                    DecorativeGeometry dg;
+                    for (int idx = 0; idx < adg.size(); idx++) {
+                        dg = adg.getElt(idx);
+                        String geomId =comp.getPathName().concat(String.valueOf(dg.getIndexOnBody()));
+                         // pad to 4 hex digits
+                        UUID uuid = UUID.randomUUID();
+                        mapGeometryToUUID.put(geomId, uuid);
+                        System.out.println("hexGeom uuid:"+uuid+" type="+dg.toString());                       
+                        dgimp.setGeomID(uuid);
+                        dg.implementGeometry(dgimp);
+                        // add entry for corresponding Material here
+                        UUID uuid_mat = UUID.randomUUID();
+                        mapMaterialToUUID.put(geomId, uuid_mat);
+                        addMaterialJsonForGeometry(uuid_mat, dg, json_materials);
+                    }
                 }
-            }   
-            out = new BufferedWriter(new FileWriter("C:/Demo40/test_2.json", true));
-            jsona.writeJSONString(out);
-            ArrayDecorativeGeometry avdg = new ArrayDecorativeGeometry();
-            model.generateDecorations(false, mdh, model.getWorkingState(), avdg);
-            if (avdg.size()>0){  // Component has some geometry
-                DecorativeGeometry dg;
-                for (int idx=0; idx <avdg.size(); idx++){
-                    dg = adg.getElt(idx);
-                    //dg.implementGeometry(dgi);
-                }
+                mcIter.next();
             }
+            JSONArray json_scene = new JSONArray();
+            // Create top level node for one group at ground frame
+            createDefaultScene(json_scene, json_geometries, json_materials);
+            
+            out.flush();
+            out.close();
         } catch (IOException ex) {
             Exceptions.printStackTrace(ex);
         } finally {
-            try {
-                out.close();
-            } catch (IOException ex) {
-                Exceptions.printStackTrace(ex);
+        }
+    }
+
+    private void addMaterialJsonForGeometry(UUID uuid_mat, DecorativeGeometry dg, JSONArray json_materials) {
+        Map mat_json = new LinkedHashMap();
+        mat_json.put("uuid", uuid_mat);
+        /* "type": "MeshPhongMaterial",
+			"color": 16711680,
+			"emissive": 8453888,
+			"specular": 16777215,
+			"shininess": 30,
+			"vertexColors": 1,
+			"shading": 1,
+			"transparent": true */
+        mat_json.put("type", "MeshPhongMaterial");
+        mat_json.put("color", mapColorToHex(dg.getColor()));
+        mat_json.put("transparent", dg.getOpacity());        
+        json_materials.add(mat_json);
+    }
+
+    private String mapColorToHex(Vec3 color) {
+        int r = (int) (color.get(0)*256);
+        int g = (int) (color.get(1)*256);
+        int b = (int) (color.get(2)*256);
+        String hexColor = String.format("#%02x%02x%02x", r, g,b);
+        return hexColor;
+    }
+
+    private void createDefaultScene(JSONArray json_scene, JSONArray json_geometries, JSONArray json_materials) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            //URI uri = t.getRequestURI();
+            File file = new File("scene/v1.html").getCanonicalFile();
+            if (!file.isFile()) {
+                // Object does not exist or is not a file: reject with 404 error.
+                String response = "404 (Not Found)\n";
+                t.sendResponseHeaders(404, response.length());
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            } else {
+                // Object exists and is a file: accept with response code 200.
+                t.sendResponseHeaders(200, 0);
+                t.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
+                OutputStream os = t.getResponseBody();
+                FileInputStream fs = new FileInputStream(file);
+                final byte[] buffer = new byte[0x10000];
+                int count = 0;
+                while ((count = fs.read(buffer)) >= 0) {
+                    os.write(buffer, 0, count);
+                }
+                fs.close();
+                os.close();
             }
         }
     }
