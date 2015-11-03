10db634be7269fa4c28be728827aecabe551f7f6
Ayman Habib <ahabib@stanford.edu>
2015-11-03 11:20:38 -0800

    Rename command to open visualizer window to VisualizerWindowAction

--- Gui/opensim/view/src/org/opensim/threejs/VisualizerWindowAction.java
+++ Gui/opensim/view/src/org/opensim/threejs/VisualizerWindowAction.java
@@ -7,17 +7,15 @@ package org.opensim.threejs;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import org.eclipse.jetty.JettyMain;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
-import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
 import org.opensim.swingui.SwingWorker;
 import org.opensim.utils.BrowserLauncher;
-import org.opensim.utils.TheApp;
 import org.opensim.view.pub.OpenSimDB;
+import org.opensim.view.pub.ViewDB;
 
 @ActionID(
         category = "Edit",
@@ -28,21 +26,15 @@ import org.opensim.view.pub.OpenSimDB;
 )
 @ActionReference(path = "Menu/Window", position = 3333)
 @Messages("CTL_VisualizerWindow=Visualizer")
-public final class VisualizerWindow implements ActionListener {
+public final class VisualizerWindowAction implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
             // launch server and open page in fixed location to be used as a view
         
-            new ExportSceneToThreeJsAction().exportCurrentModelToJson(JettyMain.getServerRootDir()+"/threejs/editor/scene.json", OpenSimDB.getInstance().getCurrentModel());
-            class VizWorker extends SwingWorker {
-                public Object construct() {
-                JettyMain.main(null);
-                    return this;
-                }
-            }
-            VizWorker viz = new VizWorker();
-            viz.start();
+            VisualizationJson json = ExportSceneToThreeJsAction.exportCurrentModelToJson(JettyMain.getServerRootDir()+"/threejs/editor/scene.json", OpenSimDB.getInstance().getCurrentModel());
+            ViewDB.getInstance().startVisualizationServer();
             BrowserLauncher.openURL("http://localhost:"+JettyMain.getServerPort()+JettyMain.getPathToStartPage()+"index.html");
+            ViewDB.getInstance().setJson(json);
     }
 }
