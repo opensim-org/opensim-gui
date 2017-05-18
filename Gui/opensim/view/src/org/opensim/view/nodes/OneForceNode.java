22347a1b284f47891d8f9bd548273ee58020677e
aymanhab <ahabib@stanford.edu>
2017-05-18 01:14:54 -0700

    Add utility class AppearanceHelper to help reuse code for Appearance edits.

--- Gui/opensim/view/src/org/opensim/view/nodes/OneForceNode.java
+++ Gui/opensim/view/src/org/opensim/view/nodes/OneForceNode.java
@@ -28,6 +28,7 @@
  */
 package org.opensim.view.nodes;
 
+import java.awt.Color;
 import java.awt.Image;
 import java.net.URL;
 import java.util.List;
@@ -35,20 +36,28 @@ import java.util.ResourceBundle;
 import javax.swing.Action;
 import javax.swing.ImageIcon;
 import org.openide.util.NbBundle;
+import org.opensim.modeling.GeometryPath;
 import org.opensim.modeling.OpenSimObject;
+import org.opensim.modeling.Vec3;
+import org.opensim.view.ColorableInterface;
 
 /**
  *
  * @author Ayman Habib
  */
-public class OneForceNode extends DisablablModelComponentNode {
+public class OneForceNode extends DisablablModelComponentNode implements ColorableInterface {
 
     private static ResourceBundle bundle = NbBundle.getBundle(OneForceNode.class);
+    private boolean hasPath = false;
+    private GeometryPath pathObject = null;
     /**
      * Creates a new instance of OneForceNode
      */
     public OneForceNode(OpenSimObject force) {
         super(force);
+        hasPath = force.hasProperty("GeometryPath");
+        if (hasPath)
+            pathObject =  GeometryPath.safeDownCast(force.getPropertyByName("GeometryPath").getValueAsObject());
 
 
 //        if (f.getDisplayer()!=null){
@@ -112,6 +121,63 @@ public class OneForceNode extends DisablablModelComponentNode {
     @Override
     public String getDisablePropertyName() {
         return ("appliesForce");
+    }   
+
+    @Override
+    public Boolean getVisible() {
+        if (hasPath)
+            return pathObject.get_Appearance().get_visible();
+        return false; // Need example Force e.g. Bushing that has Geometry other than GeometryPath
     }
-   
+
+    @Override
+    public void setVisible(Boolean newValue) {
+        if (hasPath)
+            pathObject.get_Appearance().set_visible(newValue);
+    }
+
+    @Override
+    public Color getColor() {
+        if (hasPath){
+            Vec3 c3 = pathObject.get_Appearance().get_color();
+            return new Color((float) c3.get(0), (float) c3.get(1), (float) c3.get(2));
+        }
+        else
+            return new Color(1, 1, 1);
+    }
+
+    @Override
+    public void setColor(Color newColor) {
+        if (hasPath){
+            AppearanceHelper helper = new AppearanceHelper(getModelForNode(), this, pathObject.get_Appearance());
+            helper.setAppearanceColorProperty(newColor);
+        }
+
+    }
+
+    @Override
+    public int getDisplayPreference() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setDisplayPreference(int newPref) {
+         if (hasPath){
+            AppearanceHelper helper = new AppearanceHelper(getModelForNode(), this, pathObject.get_Appearance());
+            helper.setAppearanceDisplayPrefProperty(newPref);
+        }
+    }
+
+    @Override
+    public double getOpacity() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setOpacity(double opacity) {
+          if (hasPath){
+            AppearanceHelper helper = new AppearanceHelper(getModelForNode(), this, pathObject.get_Appearance());
+            helper.setAppearanceOpacityProperty(opacity);
+        }
+   }
 }
