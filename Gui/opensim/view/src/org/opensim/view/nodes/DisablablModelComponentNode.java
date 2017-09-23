4b7e7be2365ef5c75d003cf75c3fb55a6a0fe270
Christopher Dembia <cld72@cornell.edu>
2017-09-22 18:09:20 -0700

    First attempt at applying Apache license.

--- Gui/opensim/view/src/org/opensim/view/nodes/DisablablModelComponentNode.java
+++ Gui/opensim/view/src/org/opensim/view/nodes/DisablablModelComponentNode.java
@@ -1,3 +1,25 @@
+/* -------------------------------------------------------------------------- *
+ * OpenSim: DisablablModelComponentNode.java                                  *
+ * -------------------------------------------------------------------------- *
+ * OpenSim is a toolkit for musculoskeletal modeling and simulation,          *
+ * developed as an open source project by a worldwide community. Development  *
+ * and support is coordinated from Stanford University, with funding from the *
+ * U.S. NIH and DARPA. See http://opensim.stanford.edu and the README file    *
+ * for more information including specific grant numbers.                     *
+ *                                                                            *
+ * Copyright (c) 2005-2017 Stanford University and the Authors                *
+ * Author(s): Ayman Habib                                                     *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
+ * not use this file except in compliance with the License. You may obtain a  *
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0          *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ * -------------------------------------------------------------------------- */
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
