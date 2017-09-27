a5883ef107cf134a8865aa0a496485b67d988fc9
Christopher Dembia <cld72@cornell.edu>
2017-09-26 19:40:30 -0700

    Replace TreeTableModel files with permissively licensed versions.

--- Gui/opensim/view/src/org/opensim/view/editors/TreeTableModel.java
+++ Gui/opensim/view/src/org/opensim/view/editors/TreeTableModel.java
@@ -1,43 +1,48 @@
-package org.opensim.view.editors;
 /*
  * TreeTableModel.java
+ * 
+ * Copyright 1998 Sun Microsystems, Inc.  All Rights Reserved.
  *
- * Copyright (c)  1998 Sun Microsystems, Inc. All Rights Reserved.
-* Use of the OpenSim software in source form is permitted provided that the following
-* conditions are met:
-* 	1. The software is used only for non-commercial research and education. It may not
-*     be used in relation to any commercial activity.
-* 	2. The software is not distributed or redistributed.  Software distribution is allowed 
-*     only through https://simtk.org/home/opensim.
-* 	3. Use of the OpenSim software or derivatives must be acknowledged in all publications,
-*      presentations, or documents describing work in which OpenSim or derivatives are used.
-* 	4. Credits to developers may not be removed from executables
-*     created from modifications of the source.
-* 	5. Modifications of source code must retain the above copyright notice, this list of
-*     conditions and the following disclaimer. 
-* 
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
-*  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-*  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
-*  SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-*  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-*  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-*  OR BUSINESS INTERRUPTION) OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
-*  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Sun Microsystems nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.opensim.view.editors;
 
 import javax.swing.tree.TreeModel;
 
 /**
  * TreeTableModel is the model used by a JTreeTable. It extends TreeModel
- * to add methods for getting inforamtion about the set of columns each
- * node in the TreeTableModel may have. Each column, like a column in
- * a TableModel, has a name and a type associated with it. Each node in
- * the TreeTableModel can return a value for each of the columns and
- * set that value if isCellEditable() returns true.
+ * to add methods for getting inforamtion about the set of columns each 
+ * node in the TreeTableModel may have. Each column, like a column in 
+ * a TableModel, has a name and a type associated with it. Each node in 
+ * the TreeTableModel can return a value for each of the columns and 
+ * set that value if isCellEditable() returns true. 
  *
- * @author Philip Milne
+ * @author Philip Milne 
  * @author Scott Violet
  */
 public interface TreeTableModel extends TreeModel
@@ -58,19 +63,19 @@ public interface TreeTableModel extends TreeModel
     public Class getColumnClass(int column);
 
     /**
-     * Returns the value to be displayed for node <code>node</code>,
+     * Returns the value to be displayed for node <code>node</code>, 
      * at column number <code>column</code>.
      */
     public Object getValueAt(Object node, int column);
 
     /**
-     * Indicates whether the the value for node <code>node</code>,
+     * Indicates whether the the value for node <code>node</code>, 
      * at column number <code>column</code> is editable.
      */
     public boolean isCellEditable(Object node, int column);
 
     /**
-     * Sets the value for node <code>node</code>,
+     * Sets the value for node <code>node</code>, 
      * at column number <code>column</code>.
      */
     public void setValueAt(Object aValue, Object node, int column);
--- Gui/opensim/view/src/org/opensim/view/editors/TreeTableModelAdapter.java
+++ Gui/opensim/view/src/org/opensim/view/editors/TreeTableModelAdapter.java
@@ -1,33 +1,51 @@
-package org.opensim.view.editors;
 /*
- * @(#)TreeTableModelAdapter.java	1.2 98/10/27
+ * @(#)TreeTableModelAdapter.java       1.2 98/10/27
  *
- * Copyright 1997, 1998 by Sun Microsystems, Inc.,
- * 901 San Antonio Road, Palo Alto, California, 94303, U.S.A.
- * All rights reserved.
+ * Copyright 1997, 1998 Sun Microsystems, Inc.  All Rights Reserved.
  *
- * This software is the confidential and proprietary information
- * of Sun Microsystems, Inc. ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with Sun.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Sun Microsystems nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.opensim.view.editors;
 
 import javax.swing.JTree;
 import javax.swing.SwingUtilities;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.tree.TreePath;
 import javax.swing.event.TreeExpansionEvent;
 import javax.swing.event.TreeExpansionListener;
 import javax.swing.event.TreeModelEvent;
 import javax.swing.event.TreeModelListener;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.TableModel;
-import javax.swing.tree.TreePath;
 
 /**
- * This is a wrapper class takes a TreeTableModel and implements
- * the table model interface. The implementation is trivial, with
- * all of the event dispatching support provided by the superclass:
- * the AbstractTableModel.
+ * This is a wrapper class takes a TreeTableModel and implements 
+ * the table model interface. The implementation is trivial, with 
+ * all of the event dispatching support provided by the superclass: 
+ * the AbstractTableModel. 
  *
  * @version 1.2 10/27/98
  *
@@ -38,80 +56,79 @@ public class TreeTableModelAdapter extends AbstractTableModel
 {
     JTree tree;
     TreeTableModel treeTableModel;
-    TableModel _objectProperties = null;
 
     public TreeTableModelAdapter(TreeTableModel treeTableModel, JTree tree) {
         this.tree = tree;
         this.treeTableModel = treeTableModel;
 
-	tree.addTreeExpansionListener(new TreeExpansionListener() {
-	    // Don't use fireTableRowsInserted() here; the selection model
-	    // would get updated twice.
-	    public void treeExpanded(TreeExpansionEvent event) {
-	      fireTableDataChanged();
-	    }
-            public void treeCollapsed(TreeExpansionEvent event) {
-	      fireTableDataChanged();
-	    }
-	});
+        tree.addTreeExpansionListener(new TreeExpansionListener() {
+            // Don't use fireTableRowsInserted() here; the selection model
+            // would get updated twice. 
+            public void treeExpanded(TreeExpansionEvent event) {  
+              fireTableDataChanged(); 
+            }
+            public void treeCollapsed(TreeExpansionEvent event) {  
+              fireTableDataChanged(); 
+            }
+        });
 
-	// Install a TreeModelListener that can update the table when
-	// tree changes. We use delayedFireTableDataChanged as we can
-	// not be guaranteed the tree will have finished processing
-	// the event before us.
-	treeTableModel.addTreeModelListener(new TreeModelListener() {
-	    public void treeNodesChanged(TreeModelEvent e) {
-		delayedFireTableDataChanged();
-	    }
+        // Install a TreeModelListener that can update the table when
+        // tree changes. We use delayedFireTableDataChanged as we can
+        // not be guaranteed the tree will have finished processing
+        // the event before us.
+        treeTableModel.addTreeModelListener(new TreeModelListener() {
+            public void treeNodesChanged(TreeModelEvent e) {
+                delayedFireTableDataChanged();
+            }
 
-	    public void treeNodesInserted(TreeModelEvent e) {
-		delayedFireTableDataChanged();
-	    }
+            public void treeNodesInserted(TreeModelEvent e) {
+                delayedFireTableDataChanged();
+            }
 
-	    public void treeNodesRemoved(TreeModelEvent e) {
-		delayedFireTableDataChanged();
-	    }
+            public void treeNodesRemoved(TreeModelEvent e) {
+                delayedFireTableDataChanged();
+            }
 
-	    public void treeStructureChanged(TreeModelEvent e) {
-		delayedFireTableDataChanged();
-	    }
-	});
+            public void treeStructureChanged(TreeModelEvent e) {
+                delayedFireTableDataChanged();
+            }
+        });
     }
 
-    // Wrappers, implementing TableModel interface.
+    // Wrappers, implementing TableModel interface. 
 
     public int getColumnCount() {
-	return treeTableModel.getColumnCount();
+        return treeTableModel.getColumnCount();
     }
 
     public String getColumnName(int column) {
-	return treeTableModel.getColumnName(column);
+        return treeTableModel.getColumnName(column);
     }
 
     public Class getColumnClass(int column) {
-	return treeTableModel.getColumnClass(column);
+        return treeTableModel.getColumnClass(column);
     }
 
     public int getRowCount() {
-	return tree.getRowCount();
+        return tree.getRowCount();
     }
 
     // Made public -- Eran 07/2007
     public Object nodeForRow(int row) {
-	TreePath treePath = tree.getPathForRow(row);
-	return treePath.getLastPathComponent();
+        TreePath treePath = tree.getPathForRow(row);
+        return treePath.getLastPathComponent();         
     }
 
     public Object getValueAt(int row, int column) {
-	return treeTableModel.getValueAt(nodeForRow(row), column);
+        return treeTableModel.getValueAt(nodeForRow(row), column);
     }
 
     public boolean isCellEditable(int row, int column) {
-         return treeTableModel.isCellEditable(nodeForRow(row), column);
+         return treeTableModel.isCellEditable(nodeForRow(row), column); 
     }
 
     public void setValueAt(Object value, int row, int column) {
-	treeTableModel.setValueAt(value, nodeForRow(row), column);
+        treeTableModel.setValueAt(value, nodeForRow(row), column);
     }
 
     /**
@@ -119,11 +136,10 @@ public class TreeTableModelAdapter extends AbstractTableModel
      * processed. SwingUtilities.invokeLater is used to handle this.
      */
     protected void delayedFireTableDataChanged() {
-	SwingUtilities.invokeLater(new Runnable() {
-	    public void run() {
-		fireTableDataChanged();
-	    }
-	});
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                fireTableDataChanged();
+            }
+        });
     }
 }
-
