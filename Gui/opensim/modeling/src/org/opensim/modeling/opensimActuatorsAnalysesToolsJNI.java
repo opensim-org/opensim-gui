8e0d2161e52a2082b4ee8042ec4c6fe35b118fd6
jimmyDunne <james.dunne@stanford.edu>
2018-07-25 11:02:18 -0400

    Changed URLs to point at display/OpenSim40/

--- Gui/opensim/modeling/src/org/opensim/modeling/opensimActuatorsAnalysesToolsJNI.java
+++ Gui/opensim/modeling/src/org/opensim/modeling/opensimActuatorsAnalysesToolsJNI.java
@@ -34,9 +34,9 @@ public class opensimActuatorsAnalysesToolsJNI {
           String javaHome = System.getProperties().getProperty("java.home");
           boolean inMatlab = javaHome.toLowerCase().indexOf("matlab") >= 0;
           if (inMatlab) {
-              msg +=  "\nSee https://simtk-confluence.stanford.edu/display/OpenSim/Scripting+with+Matlab";
+              msg +=  "\nSee https://simtk-confluence.stanford.edu/display/OpenSim40/Scripting+with+Matlab";
           }
-          
+
           System.out.println(msg);
           String title = "Error: Failed to load OpenSim libraries";
           if (!GraphicsEnvironment.isHeadless()) {
