220354d19d23081d3041979f7e69fff6da76763b
aymanhab <ahabib@stanford.edu>
2018-10-26 10:26:07 -0700

    Allow states files from earlier beta to be loaded in GUI

--- Gui/opensim/view/src/org/opensim/view/motions/MotionsDB.java
+++ Gui/opensim/view/src/org/opensim/view/motions/MotionsDB.java
@@ -194,8 +194,10 @@ public class MotionsDB extends Observable // Observed by other entities in motio
       int numCoordinates = cs.getSize();
       int numUsedColumns = 0;    // Keep track of how many columns correspond to Coords or Markers
       for(int i=0; i<numCoordinates; i++){
+          String testName = cs.get(i).getRelativePathName(modelForMotion);
          if (newMotion.getStateIndex(cs.get(i).getName())!=-1 ||
-                 newMotion.getStateIndex(cs.get(i).getRelativePathName(modelForMotion)+"/value")!= -1){
+                 newMotion.getStateIndex(testName+"/value")!= -1 ||
+                 testName.length() > 9 && newMotion.getStateIndex(testName.substring(9)+"/value")!= -1) {
             numUsedColumns++;
             return true;
          }
