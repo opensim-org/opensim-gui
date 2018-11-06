34fc4a9b5e2e815b2f11013f471625f306cce4c1
Ajay Seth <aseth@stanford.edu>
2018-11-05 16:33:21 -0800

    Remove empty line.

--- Gui/opensim/helpUtils/src/org/opensim/helputils/helpmenu/AboutJPanel.java
+++ Gui/opensim/helpUtils/src/org/opensim/helputils/helpmenu/AboutJPanel.java
@@ -295,7 +295,7 @@ public final class AboutJPanel extends javax.swing.JPanel implements ImageObserv
         jTextArea5.setFont(new java.awt.Font("Tahoma", 0, 11)); // NOI18N
         jTextArea5.setLineWrap(true);
         jTextArea5.setRows(5);
-        jTextArea5.setText("Please help sustain the OpenSim project by citing the following publications in your work:\n \n* OpenSim: Open-source software to create and analyze dynamic simulations of movement. IEEE T Biomed Eng, 2007.\n* OpenSim: a musculoskeletal modeling and simulation framework for in silico investigations and exchange. Procedia IUTAM, 2011.\n* What is a Moment Arm? ASME IDETC/CIE 2013\n* OpenSim: Simulating musculoskeletal dynamics and neuromuscular control to study human and animal movement. PLOS Comp Biol, 2018.\n\nFor more details see http://opensim.stanford.edu. ");
+        jTextArea5.setText("Please help sustain the OpenSim project by citing the following publications in your work:\n * OpenSim: Open-source software to create and analyze dynamic simulations of movement. IEEE T Biomed Eng, 2007.\n * OpenSim: a musculoskeletal modeling and simulation framework for in silico investigations and exchange. Procedia IUTAM, 2011.\n * What is a Moment Arm? ASME IDETC/CIE 2013\n * OpenSim: Simulating musculoskeletal dynamics and neuromuscular control to study human and animal movement. PLOS Comp Biol, 2018.\n\nFor more details see http://opensim.stanford.edu.");
         jTextArea5.setWrapStyleWord(true);
         jTextArea5.setAlignmentY(0.0F);
         jTextArea5.setMargin(new java.awt.Insets(0, 5, 0, 0));
