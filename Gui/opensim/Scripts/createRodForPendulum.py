73514bef3f6d7ad62bf054c710a0e31b0e9d001c
Ajay Seth <aseth@stanford.edu>
2018-11-09 19:57:02 -0800

    Update comments for creating single rod.

--- Gui/opensim/Scripts/createRodForPendulum.py
+++ Gui/opensim/Scripts/createRodForPendulum.py
@@ -1,14 +1,14 @@
-#createRod1ForPendulum.py
-
 # Create an empty model for pendulum
 model = modeling.Model()
 model.setName("pendulum")
+
+# get the model's Ground (inertial) reference frame
 ground = model.getGround()
 
 # define rod dimensions
 length = 1.0;
 radius = 0.025;
 
-# add body segments which are the rigid rods of the pendulum
+# add rigid rod (Body) of the pendulum with cylinder geometry
 rod = addBodyToModel(model, 'rod')
 attachGeometryWithOffset(rod, modeling.Cylinder(radius, length/2))
