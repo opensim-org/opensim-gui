2093e32b75176b2e03e33a06e9be65713ef3a325
aymanhab <ahabib@stanford.edu>
2019-10-01 16:16:09 -0700

    Allow GeometryPath objects to provide their own color irrespective of wether owned by a Muscle

--- Gui/opensim/view/src/org/opensim/threejs/ModelVisualizationJson.java
+++ Gui/opensim/view/src/org/opensim/threejs/ModelVisualizationJson.java
@@ -791,7 +791,7 @@ public class ModelVisualizationJson extends JSONObject {
                 UUID pathUUID = pathList.get(geomPathObject);
                 JSONObject pathUpdate_json = new JSONObject();
                 pathUpdate_json.put("uuid", pathUUID.toString());
-                if (Muscle.safeDownCast(geomPathObject.getOwner())!= null){
+                // Allow non-muscles to provide colors if (Muscle.safeDownCast(geomPathObject.getOwner())!= null){
                     Vec3 pathColor = colorByState ? currentPathColorMap.getColor(geomPathObject, state, -1) : geomPathObject.getDefaultColor();
                 
                     if (verbose)
@@ -799,7 +799,7 @@ public class ModelVisualizationJson extends JSONObject {
                     String colorString = JSONUtilities.mapColorToRGBA(pathColor);
                     pathUpdate_json.put("color", colorString);
                     geompaths_json.add(pathUpdate_json);
-                }
+                //}
             }
             // Have AddOns update their tranasforms
             for (VisualizerAddOn nextAddOn:visualizerAddOns){
