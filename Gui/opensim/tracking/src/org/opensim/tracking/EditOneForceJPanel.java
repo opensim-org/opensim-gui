03ac96e567bdf7b9349451cfccd1a20ccdbdb053
aymanhab <ahabib@stanford.edu>
2019-11-20 09:09:56 -0800

    Repeat the fix for applies Torque checkbox

--- Gui/opensim/tracking/src/org/opensim/tracking/EditOneForceJPanel.java
+++ Gui/opensim/tracking/src/org/opensim/tracking/EditOneForceJPanel.java
@@ -686,8 +686,14 @@ public class EditOneForceJPanel extends javax.swing.JPanel {
                     (String)jComboBoxTY.getSelectedItem(), 
                     (String)jComboBoxTZ.getSelectedItem()));
             //if (!initializing) externalForce.setTorqueFunctionNames(torqueFunctionNames, forceStorage);
-        } else
-            ;//if (!initializing) externalForce.clearTorqueFunctions();
+        } else {
+            if (!initializing) {
+                externalForce.setTorqueIdentifier("");
+                jComboBoxTX.setSelectedItem("");
+                jComboBoxTY.setSelectedItem("");
+                jComboBoxTZ.setSelectedItem("");
+            }
+        }
         String selected = (String) ForceExpressedBodiesComboBox.getSelectedItem();
         if (selected !=null)
             externalForce.setForceExpressedInBodyName(selected);
