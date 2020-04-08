fd3049bf62c844868724ed2aed5e087cc65f8303
aymanhab <ahabib@stanford.edu>
2020-04-08 15:35:40 -0700

    Remove bogus Deg./Rad. from display names in Plotter

--- Gui/opensim/plotter/src/org/opensim/plotter/PlotterSourceMotion.java
+++ Gui/opensim/plotter/src/org/opensim/plotter/PlotterSourceMotion.java
@@ -61,9 +61,5 @@ public class PlotterSourceMotion extends PlotterSourceStorage {
    void updateMotionName() {  // Update display name after a motion rename
      displayName = storage.getName();
    }
-
-    public String getDisplayName() {
-        return displayName+"(Deg.)";
-    }
    
 }
