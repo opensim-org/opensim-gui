89b42ad8ead691d5cf44e4aff4ed84921b20dd69
aymanhab <ahabib@stanford.edu>
2020-10-14 15:30:36 -0700

    Use legacy Command mechanism and layer.xml so that commands can be enabled/disabled

--- Gui/opensim/tracking/src/org/opensim/tracking/tools/IMUCalibrateAction.java
+++ Gui/opensim/tracking/src/org/opensim/tracking/tools/IMUCalibrateAction.java
@@ -5,6 +5,9 @@
  */
 package org.opensim.tracking.tools;
 
+import org.openide.util.actions.CallableSystemAction;
+import org.openide.util.NbBundle;
+import org.openide.util.HelpCtx;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -18,7 +21,7 @@ import org.opensim.tracking.IMUCalibrationPanel;
 import org.opensim.tracking.IMUIKToolPanel;
 import org.opensim.utils.ErrorDialog;
 import org.opensim.view.pub.OpenSimDB;
-
+/*
 @ActionID(
         category = "Tools",
         id = "org.opensim.tracking.tools.IMUCalibrateAction"
@@ -28,10 +31,11 @@ import org.opensim.view.pub.OpenSimDB;
 )
 @ActionReference(path = "Menu/Tools", position = 190, separatorBefore = 150)
 @Messages("CTL_IMUCalibrateAction=IMU Calibrate...")
-public final class IMUCalibrateAction implements ActionListener {
+*/
+public final class IMUCalibrateAction  extends CallableSystemAction {
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void performAction() {
         // TODO implement action body      
       Model model = OpenSimDB.getInstance().getCurrentModel();
       if(model==null) return;
@@ -40,8 +44,27 @@ public final class IMUCalibrateAction implements ActionListener {
          final IMUCalibrationPanel panel = new IMUCalibrationPanel(model);
          BaseToolPanel.openToolDialog(panel, "IMU Model Calibration Tool");
       } catch (IOException ex) {
-         ErrorDialog.displayIOExceptionDialog("Inverse Kinematics Tool Error","Error while initializing inverse kinematics tool",ex);
+         ErrorDialog.displayIOExceptionDialog("IMU Model Calibration Tool Error","Error while initializing IMUCalibrateAction",ex);
       }
-
     }
+       public String getName() {
+      return NbBundle.getMessage(IMUCalibrateAction.class, "CTL_IMUCalibrateAction");
+   }
+   
+   protected void initialize() {
+      super.initialize();
+      // see org.openide.util.actions.SystemAction.iconResource() javadoc for more details
+      putValue("noIconInMenu", Boolean.TRUE);
+   }
+   
+   protected boolean asynchronous() {
+      return false;
+   }
+   public HelpCtx getHelpCtx() {
+      return HelpCtx.DEFAULT_HELP;
+   }
+   public boolean isEnabled() {
+      return (OpenSimDB.getInstance().getCurrentModel()!=null);
+   }
+
 }
--- Gui/opensim/tracking/src/org/opensim/tracking/tools/IMUIKToolAction.java
+++ Gui/opensim/tracking/src/org/opensim/tracking/tools/IMUIKToolAction.java
@@ -5,42 +5,51 @@
  */
 package org.opensim.tracking.tools;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
-import org.openide.awt.ActionID;
-import org.openide.awt.ActionReference;
-import org.openide.awt.ActionRegistration;
-import org.openide.util.NbBundle.Messages;
+import org.openide.util.actions.CallableSystemAction;
+import org.openide.util.NbBundle;
+import org.openide.util.HelpCtx;
 import org.opensim.modeling.Model;
 import org.opensim.tracking.BaseToolPanel;
 import org.opensim.tracking.IMUIKToolPanel;
 import org.opensim.utils.ErrorDialog;
 import org.opensim.view.pub.OpenSimDB;
 
-@ActionID(
-        category = "Tools",
-        id = "org.opensim.tracking.tools.IMUIKToolAction"
-)
-@ActionRegistration(
-        displayName = "#CTL_IMUIKToolAction"
-)
-@ActionReference(path = "Menu/Tools", position = 200, separatorAfter = 250)
-@Messages("CTL_IMUIKToolAction=IMU Inverse Kinematics...")
-public final class IMUIKToolAction implements ActionListener {
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+public final class IMUIKToolAction extends CallableSystemAction {
+   public void performAction() {
         // TODO implement action body      
-      Model model = OpenSimDB.getInstance().getCurrentModel();
-      if(model==null) return;
+        Model model = OpenSimDB.getInstance().getCurrentModel();
+        if (model == null) {
+            return;
+        }
 
-      try {
-         final IMUIKToolPanel panel = new IMUIKToolPanel(model);
-         BaseToolPanel.openToolDialog(panel, "IMU Inverse Kinematics Tool");
-      } catch (IOException ex) {
-         ErrorDialog.displayIOExceptionDialog("Inverse Kinematics Tool Error","Error while initializing inverse kinematics tool",ex);
-      }
-        
+        try {
+            final IMUIKToolPanel panel = new IMUIKToolPanel(model);
+            BaseToolPanel.openToolDialog(panel, "IMU Inverse Kinematics Tool");
+        } catch (IOException ex) {
+            ErrorDialog.displayIOExceptionDialog("Inverse Kinematics Tool Error", "Error while initializing inverse kinematics tool", ex);
+        }
     }
+   public String getName() {
+      return NbBundle.getMessage(IMUIKToolAction.class, "CTL_IMUIKToolAction");
+   }
+   
+   protected void initialize() {
+      super.initialize();
+      // see org.openide.util.actions.SystemAction.iconResource() javadoc for more details
+      putValue("noIconInMenu", Boolean.TRUE);
+   }
+   
+   protected boolean asynchronous() {
+      return false;
+   }
+   public HelpCtx getHelpCtx() {
+      return HelpCtx.DEFAULT_HELP;
+   }
+   public boolean isEnabled() {
+      return (OpenSimDB.getInstance().getCurrentModel()!=null &&
+              OpenSimDB.getInstance().getCurrentModel().getNumCoordinates()>=1);
+   }
+
 }
