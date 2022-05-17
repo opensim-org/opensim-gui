d2b989befbdfbf7004c27ae0b6588056e23faec1
Ayman Habib <ahabib@stanford.edu>
2022-05-17 12:14:43 -0700

    Avoid overriding handling of path-segments with conditional path points

--- Gui/opensim/view/src/org/opensim/threejs/ModelVisualizationJson.java
+++ Gui/opensim/view/src/org/opensim/threejs/ModelVisualizationJson.java
@@ -1689,11 +1689,12 @@ public class ModelVisualizationJson extends JSONObject {
                 pathpointActive_jsonArr.add(false);
                 pathpoint_jsonArr.add(pathpoint_uuid.toString());
                 // traverse backward to an active point
+                /*
                 int tempIndex = ppointSetIndex;
                 while (!pathPointSetFromProperty.get(tempIndex).isActive(state))
                     tempIndex--;
                 secondPoint = pathPointSetFromProperty.get(tempIndex);
-                secondIndex = tempIndex;
+                secondIndex = tempIndex;*/
             }
             else { // Wrapping encountered in createJsonForGeometryPath
                 int numWrapPoints = secondIndex - firstIndex -1;
